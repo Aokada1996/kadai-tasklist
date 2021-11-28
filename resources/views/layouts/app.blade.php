d3541d7b18fa5fafff0f937fbb7fa9acf80f3543
ayaka.okada <testttt@test.co.jp>
2021-11-28 12:47:04 +0000

    add column

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,15 @@
                 </div>
             </nav>
         </header>
+        
+        @if (count($errors) > 0)
+            <ul class="alert alert-danger" role="alert">
+        @foreach ($errors->all() as $error)
+            <li class="ml-4">{{ $error }}</li>
+        @endforeach
+        </ul>
+        @endif
+
 
         <div class="container">
             @yield('content')
